f5e2b2eef868d060ede3abf61e1dcfcb0051c8ac
Hongliang Wang <hongliang7876@gmail.com>
2017-02-23 12:23:27 -0500

    Modified the index.js and the index.ejs to navigate to pages

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,6 +14,28 @@ router.get('/register', function(req, res) {
 
 router.get('/aboutUs', function(req, res) {
 	res.render('aboutUs');
+});
+
+router.get('/event', function(req, res) {
+	res.render('event', {
+		siteTitle: 'Ezra Fellowship',
+		pageTitle: 'Event',
+		info: 'The page is coming soon'
+	});
+});
+router.get('/bibleStudy', function(req, res) {
+	res.render('bibleStudy', {
+		siteTitle: 'Ezra Fellowship',
+		pageTitle: 'Event',
+		info: 'The page is coming soon'
+	});
+});
+router.get('/devotion', function(req, res) {
+	res.render('devotion', {
+		siteTitle: 'Ezra Fellowship',
+		pageTitle: 'Event',
+		info: 'The page is coming soon'
+	});
 })
 
 
